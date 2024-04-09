282fb3eb5ec13d45bb91ce26b7f684be25528b93
Adrianjg18 <adriangomezcatacora@gmail.com>
2024-04-09 11:56:38 -0400

    create careers migrations

---
+++ database/migrations/2024_04_08_045653_create_careers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->idCareer();
+            $table->string('name_career', 50);
+            $table->string('phone', 10);
+            $table->BigInteger('idUniversity')->unsigned();
+            $table->foreing('idUniversity')->references('id')->on('university');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
