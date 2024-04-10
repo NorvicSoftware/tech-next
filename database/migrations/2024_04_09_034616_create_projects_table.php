f48bbade81dc03802ecf7b9ef4c7bc33a3069dd0
EA-Tibubay <angeltibubay@gmail.com>
2024-04-10 00:42:04 -0400

    modificacion de la migracion projects

--- database/migrations/2024_04_09_034616_create_projects_table.php
+++ database/migrations/2024_04_09_034616_create_projects_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('proyects', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
 			$table->string('title_project', 250);
             $table->integer('note');
