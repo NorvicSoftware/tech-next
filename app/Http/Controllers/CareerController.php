a1f57285bdcecbe002b402a617f3e37913969a81
Adrianjg18 <adriangomezcatacora@gmail.com>
2024-04-10 19:35:06 -0400

    create career controller with CRUD

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -3,23 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Career;
 
 class CareerController extends Controller
 {
-    /**
+        /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
+    
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // Mostrar todas las carreras
+        $career = Career::all();
+        return response()->json($career);
     }
 
     /**
@@ -27,23 +27,37 @@ class CareerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Definir reglas de validación para los datos de la carrera
+        $validated = $request->validate([
+            'name' => 'required|max:50',
+            'phone' => 'max:10',
+            'university.id' => 'required|integer|exists:universities,id'
+        ]);
+        DB::beginTransaction(); //Iniciar transacciones
+        try {
+            $career = new Career();
+            $career->name = $request->name;
+            $career->phone = $request->phone;
+            $career->university_id = $request->university['id'];
+            $career->save();
+            DB::commit(); //Aplica los cambios realizados a la BD
+            return response()->json(['status' => true, 'message' => 'La carrera' . $request->career['name'] . 'fue creada exitosamente']);
+        }catch (\Exception $exc){
+            DB::rollback(); // Evita que se apliquen cambios parciales a l BD
+            return response()->json(['status' => false, 'message' => 'Error al crear la carrera' .$exc]);
+        }
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        $career = Career::with(['university'])->where('id', '=', $id)->get();
+        return response()->json($career);
     }
 
     /**
@@ -51,7 +65,23 @@ class CareerController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|max:50',
+            'phone' => 'max:10',
+            'university.id' => 'required|integer|exists:universities,id'
+        ]);
+        try {
+            $career = Career::findOrFail($id);
+            $career->name = $request->name;
+            $career->phone = $request->phone;
+            $career->university_id = $request->university['id'];
+            $career->save();
+            DB::commit(); //Aplica los cambios realizados a la BD
+            return response()->json(['status' => true, 'message' => 'La carrera' . $request->career['name'] . 'fue actualizada exitosamente']);
+        }catch (\Exception $exc){
+            DB::rollback(); // Evita que se apliquen cambios parciales a l BD
+            return response()->json(['status' => false, 'message' => 'Error al editar la carrera']);
+        }
     }
 
     /**
@@ -59,6 +89,14 @@ class CareerController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $career = Career::findOrFail($id);
+            $career->delete();
+            DB::commit(); //Aplica los cambios realizados a la BD
+            return response()->json(['status' => true, 'message' => 'La carrera' . $request->career['name'] . 'fue eliminada exitosamente']);
+        }catch (\Exception $exc){
+            DB::rollback(); // Evita que se apliquen cambios parciales a l BD
+            return response()->json(['status' => false, 'message' => 'Error al eliminar la carrera']);
+        }
     }
 }
