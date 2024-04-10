11b2f89d21ec7e9f9b52907963bdeba76402990d
arrozconpapas <victorseleme09@gmail.com>
2024-04-10 13:19:28 -0400

    se arreglo la relacion de modelos univerity y career

--- app/Models/Career.php
+++ app/Models/Career.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Career extends Model
 {
     use HasFactory;
 
-    protected $table = 'career';
-    public function university()
-    {
-        return $this->BelonsTo(University::class);
+    protected $table = 'careers';
+    public function university(): BelongsTo{
+        return $this->belongsTo(University::class);
     }
+    protected $fillable = [
+        'name', 'phone', 'university_id'
+    ];
 }
