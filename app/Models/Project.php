64ca949f5570de3125ddf8b99df616e66946dfdf
victor <victorpenia.vhpq@gmail.com>
2024-04-10 12:57:05 -0400

    Added migration and model to score

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -11,5 +12,9 @@ class Project extends Model
     protected $table = 'projects';
 
     protected $fillable = ['title', 'year'];
+
+    public function scores(): HasMany {
+        return $this->hasMany(Score::class);
+    }
     
 }
---
+++ database/migrations/2024_04_10_164016_create_scores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->enum('reaction', ['1' , '2', '3']);
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('proyects')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scores');
+    }
+};
