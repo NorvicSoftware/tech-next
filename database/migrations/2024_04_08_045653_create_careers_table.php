276d64a9614f72a256ff980d9a1c42b1a05e2b06
Adrian Gomez <127125334+Adrianjg18@users.noreply.github.com>
2024-04-10 19:40:14 -0400

    Added a migration update

--- database/migrations/2024_04_08_045653_create_careers_table.php
+++ database/migrations/2024_04_08_045653_create_careers_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->unsignedBigInteger('university_id');
             $table->foreign("university_id")->references("id")->on("universities")->onDelete('cascade');
             $table->timestamps(); 
-            $table->timestamps();
         });
     }
 
