962ffe45235828a931962f63ed82711f608c00bd
Adrianjg18 <adriangomezcatacora@gmail.com>
2024-04-10 18:02:55 -0400

    Added a career migration update

--- database/migrations/2024_04_08_045653_create_careers_table.php
+++ database/migrations/2024_04_08_045653_create_careers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 50);
             $table->string('phone', 10)->nullable();
             $table->unsignedBigInteger('university_id');
-            $table->foreign("university_id")->references("id")->on("universitie")->onDelete('cascade');
+            $table->foreign("university_id")->references("id")->on("universities")->onDelete('cascade');
             $table->timestamps(); 
         });
     }
