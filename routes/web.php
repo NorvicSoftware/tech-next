3273819fe8f4a43e098a85ee71c1a1fcffcfc2a4
Diland-Castellón <gustavocastellon63@gmail.com>
2024-04-16 17:50:27 -0400

    .

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PersonsController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ImportController; // Exel
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -13,15 +15,28 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Persons Controller
+
+    Route::get('/persons', [PersonsController::class, 'index'])->name('persons.index');
+    Route::get('/persons/create', [PersonsController::class, 'create'])->name('persons.create');
+    Route::post('/persons', [PersonsController::class, 'store'])->name('persons.store');
+    Route::get('/persons/{id}/edit', [PersonsController::class, 'edit'])->name('persons.edit');
+    Route::put('/persons/{id}', [PersonsController::class, 'update'])->name('persons.update');
+    Route::delete('/persons/{id}', [PersonsController::class, 'destroy'])->name('persons.destroy');
+
 });
+
+//Excel
+Route::get('/import', [ImportController::class, 'showImportForm'])->name('import.form');
+Route::post('/import/projects', [ImportController::class, 'importProjects'])->name('import.projects');
+Route::post('/import/persons', [ImportController::class, 'importPersons'])->name('import.persons');
 
 require __DIR__.'/auth.php';
