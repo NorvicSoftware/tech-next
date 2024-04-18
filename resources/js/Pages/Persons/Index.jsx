c81132815f53d7c69c34e459374ffb1cd33c1815
Rey <reycrisbf@gmail.com>
2024-04-18 06:50:40 -0400

    Se hizo la union con la rama Mai-Maide para obtener la lista de personas, crear, editar y eliminar

--- resources/js/Pages/Persons/Index.jsx
+++ resources/js/Pages/Persons/Index.jsx
@@ -1,57 +1,94 @@
-//import Router from "vendor/tightenco/ziggy/src/js/Router";
-
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { usePage } from "@inertiajs/react"; //Rcuperar datos
+import { usePage } from "@inertiajs/react";
 import NavLink from "@/Components/NavLink";
 
-export default function Index (auth){
-
+export default function Index(auth) {
     const { persons } = usePage().props;
     console.log();
     return (
-
-<AuthenticatedLayout user={ auth } header="" >
-
-    <div className="border-b justify-between items-center bg-gray-50 px-4 py-1">
-        <p></p>
-        <div className=" text-left">
-        <NavLink href= {route('persons.create')} active={route().current('persons.create')} className=" mx-6 px-4 my-2 py-1 border-transparent font-medium rounded-md text-white hover:bg-blue-700 bg-blue-800" >
-            <button className="text-center text-ms">
-            + Añadir nuevo
-            </button>
-       
-        </NavLink>
-        </div>
-    </div>
-<div>
-<table className="min-w-full">
-    <thead className="border-b bg-gray-200">
-        <tr>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">id</th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Nombres</th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Apellidos</th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Acción</th>
-        </tr>
-    </thead>
-    <tbody>
-        { persons.map((person, id) => (
-            <tr key={id} className=" text-center bg-white border-b">
-                <td className="text-sm font-ligth text-gray-900 px-5 py-3">{ person.id }</td>
-                <td className="text-sm text-gray-900 font-light px-5 py-3">{ person.first_name }</td>
-                <td className="text-sm text-gray-900 font-light px-5 py-3">{ person.last_name }</td>
-                <td className="px-4 py-3 sm:px-6 sm:py-4 whitespace-nowrap"> {/*  accion */}
-                
-                <div >
-                <NavLink href={route('persons.store', person.id)} className=" mx-6 px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600">Editar</NavLink>
-
-                <NavLink href={route('persons.destroy', person.id)} method="delete" as="button" className="sx-10px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600">Eliminar</NavLink>
+        <AuthenticatedLayout user={auth} header="">
+            <div className="flex justify-between mx-10 items-center py-2">
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Autores
+                </h2>
+                <div className="text-left">
+                    <NavLink
+                        href={route("persons.create")}
+                        active={route().current("persons.create")}
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white hover:bg-blue-400"
+                    >
+                        + Añadir nuevo autor
+                    </NavLink>
                 </div>
-                </td>
-            </tr>
-        )) }
-    </tbody>
-</table>
-</div>
-</AuthenticatedLayout>
-    )
-};
+            </div>
+            <div>
+                <table className="min-w-full">
+                    <thead className="border-b bg-gray-200">
+                        <tr>
+                            <th
+                                scope="col"
+                                className="text-sm font-medium text-gray-900 px-6 py-4"
+                            >
+                                Nombres
+                            </th>
+                            <th
+                                scope="col"
+                                className="text-sm font-medium text-gray-900 px-6 py-4"
+                            >
+                                Apellidos
+                            </th>
+                            <th
+                                scope="col"
+                                className="text-sm font-medium text-gray-900 px-6 py-4"
+                            >
+                                Acción
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {persons.map((person, id) => (
+                            <tr
+                                key={id}
+                                className=" text-center bg-white border-b"
+                            >
+                                <td className="text-sm text-gray-900 font-light px-5 py-3">
+                                    {person.first_name}
+                                </td>
+                                <td className="text-sm text-gray-900 font-light px-5 py-3">
+                                    {person.last_name}
+                                </td>
+                                <td className="px-4 py-3 sm:px-6 sm:py-4 whitespace-nowrap">
+                                    {" "}
+                                    {/*  accion */}
+                                    <div>
+                                        <NavLink
+                                            href={route(
+                                                "persons.edit",
+                                                person.id
+                                            )}
+                                            className=" mx-6 px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+                                        >
+                                            Editar
+                                        </NavLink>
+
+                                        <NavLink
+                                            href={route(
+                                                "persons.destroy",
+                                                person.id
+                                            )}
+                                            method="delete"
+                                            as="button"
+                                            className="sx-10px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+                                        >
+                                            Eliminar
+                                        </NavLink>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
--- resources/js/Pages/projects/create.jsx
+++ resources/js/Pages/projects/create.jsx
@@ -1,113 +1,141 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useForm } from "@inertiajs/react";
 
 const Create = ({ auth, persons, careers }) => {
-  const { data, setData, post, errors, reset } = useForm({
-    title_project: '',
-    note: '',
-    year: '',
-    manager: '',
-    person_id: '',
-    career_id: '',
-  });
+    const { data, setData, post, errors, reset } = useForm({
+        title_project: "",
+        note: "",
+        year: "",
+        manager: "",
+        person_id: "",
+        career_id: "",
+    });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    post(route('projects.store'));
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        post(route("projects.store"));
+    };
 
-  return (
-    <AuthenticatedLayout user={auth} Header="Proyectos">
-      <div className="max-w-3xl mx-auto p-6">
-        <h1 className="text-3xl font-bold mb-6 text-white">Crear Proyecto</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label className="block mb-1 text-white">Título</label>
-            <input
-              type="text"
-              className="w-full border rounded px-3 py-2"
-              value={data.title}
-              onChange={(e) => setData('title', e.target.value)}
-            />
-            {errors.title && (
-              <span className="text-red-500">{errors.title}</span>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1 text-white">Calificación</label>
-            <textarea
-              className="w-full border rounded px-3 py-2"
-              value={data.qualification}
-              onChange={(e) => setData('qualification', e.target.value)}
-            />
-            {errors.qualification && (
-              <span className="text-red-500">{errors.qualification}</span>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1 text-white">Año</label>
-            <input
-              type="text"
-              className="w-full border rounded px-3 py-2"
-              value={data.year}
-              onChange={(e) => setData('year', e.target.value)}
-            />
-            {errors.year && (
-              <span className="text-red-500">{errors.year}</span>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1 text-white">Tutor</label>
-            <input
-              type="text"
-              className="w-full border rounded px-3 py-2"
-              value={data.manager}
-              onChange={(e) => setData('manager', e.target.value)}
-            />
-            {errors.manager && (
-              <span className="text-red-500">{errors.manager}</span>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1 text-white">Autor</label>
-            <select
-              className="w-full border rounded px-3 py-2"
-              value={data.person_id}
-              onChange={(e) => setData('person_id', e.target.value)}
-            >
-              <option value="">Select Person</option>
-              {persons.map((person) => (
-                <option key={person.id} value={person.id}>{`${person.first_name} ${person.last_name}`}</option>
-              ))}
-            </select>
-            {errors.person_id && (
-              <span className="text-red-500">{errors.person_id}</span>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1 text-white">Career:</label>
-            <select
-              className="w-full border rounded px-3 py-2"
-              value={data.career_id}
-              onChange={(e) => setData('career_id', e.target.value)}
-            >
-              <option value="">Select Career</option>
-              {careers.map((career) => (
-                <option key={career.id} value={career.id}>{career.name}</option>
-              ))}
-            </select>
-            {errors.career_id && (
-              <span className="text-red-500">{errors.career_id}</span>
-            )}
-          </div>
-          <div>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create</button>
-          </div>
-        </form>
-      </div>
-    </AuthenticatedLayout>
-  );
+    return (
+        <AuthenticatedLayout user={auth} Header="Proyectos">
+            <div className="max-w-3xl mx-auto p-6">
+                <h1 className="text-3xl font-bold mb-6 text-white">
+                    Crear Proyecto
+                </h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label className="block mb-1 text-white">Título</label>
+                        <input
+                            type="text"
+                            className="w-full border rounded px-3 py-2"
+                            value={data.title}
+                            onChange={(e) => setData("title", e.target.value)}
+                        />
+                        {errors.title && (
+                            <span className="text-red-500">{errors.title}</span>
+                        )}
+                    </div>
+                    <div>
+                        <label className="block mb-1 text-white">
+                            Calificación
+                        </label>
+                        <textarea
+                            className="w-full border rounded px-3 py-2"
+                            value={data.qualification}
+                            onChange={(e) =>
+                                setData("qualification", e.target.value)
+                            }
+                        />
+                        {errors.qualification && (
+                            <span className="text-red-500">
+                                {errors.qualification}
+                            </span>
+                        )}
+                    </div>
+                    <div>
+                        <label className="block mb-1 text-white">Año</label>
+                        <input
+                            type="text"
+                            className="w-full border rounded px-3 py-2"
+                            value={data.year}
+                            onChange={(e) => setData("year", e.target.value)}
+                        />
+                        {errors.year && (
+                            <span className="text-red-500">{errors.year}</span>
+                        )}
+                    </div>
+                    <div>
+                        <label className="block mb-1 text-white">Tutor</label>
+                        <input
+                            type="text"
+                            className="w-full border rounded px-3 py-2"
+                            value={data.manager}
+                            onChange={(e) => setData("manager", e.target.value)}
+                        />
+                        {errors.manager && (
+                            <span className="text-red-500">
+                                {errors.manager}
+                            </span>
+                        )}
+                    </div>
+                    <div>
+                        <label className="block mb-1 text-white">Autor</label>
+                        <select
+                            className="w-full border rounded px-3 py-2"
+                            value={data.person_id}
+                            onChange={(e) =>
+                                setData("person_id", e.target.value)
+                            }
+                        >
+                            <option value="">Select Person</option>
+                            {persons.map((person) => (
+                                <option
+                                    key={person.id}
+                                    value={person.id}
+                                >{`${person.first_name} ${person.last_name}`}</option>
+                            ))}
+                        </select>
+                        {errors.person_id && (
+                            <span className="text-red-500">
+                                {errors.person_id}
+                            </span>
+                        )}
+                    </div>
+                    <div>
+                        <label className="block mb-1 text-white">Career:</label>
+                        <select
+                            className="w-full border rounded px-3 py-2"
+                            value={data.career_id}
+                            onChange={(e) =>
+                                setData("career_id", e.target.value)
+                            }
+                        >
+                            <option value="">Select Career</option>
+                            {careers.map((career) => (
+                                <option key={career.id} value={career.id}>
+                                    {career.name}
+                                </option>
+                            ))}
+                        </select>
+                        {errors.career_id && (
+                            <span className="text-red-500">
+                                {errors.career_id}
+                            </span>
+                        )}
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                        >
+                            Crear
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </AuthenticatedLayout>
+    );
 };
 
 export default Create;
