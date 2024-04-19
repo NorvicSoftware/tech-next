abbd73297bde7cca27dd05b8f4e6575b14d6aec5
Rey <reycrisbf@gmail.com>
2024-04-19 19:13:27 -0400

    Se limito a 50 caracteres el campo name de CareerFactory

--- database/factories/CareerFactory.php
+++ database/factories/CareerFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\University;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Career>
@@ -12,9 +13,10 @@ class CareerFactory extends Factory
 
     public function definition(): array
     {
-        return [
+        $jobTitle = $this->faker->jobTitle();
 
-            'name' => $this->faker->jobTitle(),
+        return [
+            'name' => Str::limit($jobTitle, 50),
             'phone' => $this->faker->numberBetween(65000000, 75000000),
             'university_id' => University::all()->random()->id,
         ];
