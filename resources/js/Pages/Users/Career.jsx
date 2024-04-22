91e85277dab15226ce7c5f1b45c13164d1d3c55b
Maicol <maicolarratia4@gmail.com>
2024-04-22 11:07:49 -0400

    creat career

---
+++ resources/js/Pages/Users/Career.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+const Career = () => {
+    return (
+        <div>
+            <div class="fixed top-0 w-full h-[70px] bg-gray-900">
+                <h1 class="text-3xl font-serif text-Left text-gray-100 py-4 px-4">
+                    Tech Next
+                </h1>
+            </div>
+
+            <p class="text-gray-950 font-serif font-bold text-lg py-24 flex flex-col items-center">
+                Inspirate y crea nuevas ideas
+                <p class="py-5 text-xs"> Elige una carrera: </p>
+            </p>
+
+            <div
+                class="container mx-auto h-auto px-7 sm:p-16 grid grid-cols-2 sm:grid-cols-2 
+            md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-14 -mt-20"
+            >
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900 ">
+                    <a href="#" class="">
+                        <img
+                            src="https://44653845.fs1.hubspotusercontent-na1.net/hubfs/44653845/Imported_Blog_Media/MicrosoftTeams-image.jpg"
+                            alt=""
+                            class="h-48 w-58 "
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Ing. Sistemas
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900  ">
+                    <a href="#" class="">
+                        <img
+                            src="https://i0.wp.com/www.entreperiodistas.com/wp-content/uploads/2022/11/mujer-sonriendo-funciones-sociales-del-periodismo.jpg?fit=800%2C533&ssl=1"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Comunicación
+                        </h2>
+                    </a>
+                </div>
+
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900 ">
+                    <a href="#" class="">
+                        <img
+                            src="https://www.shutterstock.com/image-photo/warehouse-specialists-talking-about-factory-260nw-2119544510.jpg"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Ing. Comercial
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900">
+                    <a href="#" class="">
+                        <img
+                            src="https://www.toulouselautrec.edu.pe/sites/default/files/publicidad-y-marketing-digital-ventajas.jpg"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Markering
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900">
+                    <a href="#" class="">
+                        <img
+                            src="https://pablocarro.com.ar/wp-content/uploads/diseno-grafico-duracion-de-la-carrera-y-todo-lo-que-necesitas-saber.jpg"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Diseño Gráfico
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900">
+                    <a href="#" class="">
+                        <img
+                            src="https://isil.pe/blog/wp-content/uploads/2023/09/administracion-empresas.webp"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Adm. Empresas
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900">
+                    <a href="#" class="">
+                        <img
+                            src="https://www.senati.edu.pe/sites/default/files/2017/carrera/11/carreras-dual-mecatronica-industrial-senati-1800x1190.jpg"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Mecatrónica
+                        </h2>
+                    </a>
+                </div>
+                <div class="bg-slate-900 text-white rounded-lg border-4 border-slate-900">
+                    <a href="#" class="">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LwDaPBIZq2kw6cLZKpZrVe5wpsDRx57NIUqzjfdgoh2lNdKos7ndDFSYsyza6uG0Ku4&usqp=CAU"
+                            alt=""
+                            class="h-48 w-58"
+                        />
+                        <h2 class="text-lg md:text-xl font-bold text-center">
+                            Turismo
+                        </h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Career;
