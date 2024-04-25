04152bb97744858f1e91691be55774faa9487212
jhojanCT <jhojancoro1@gmail.com>
2024-04-25 11:24:20 -0400

    .

--- resources/js/Pages/Users/Project.jsx
+++ resources/js/Pages/Users/Project.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import UserLayout from "@/Layouts/UserLayout";
 import ScoreReaction from "@/Components/ScoreReaction";
-import ProjectData from "@/Components/ProjectData";
 
-const Project = () => {
+
+const Project = ({ project }) => {
     return (
         <UserLayout>
-            <div className="max-w-3xl p-6 grid justify-center my-5 border-2 rounded-xl border-black mx-3">
+            <div className="max-w-3xl mx-auto p-6 grid justify-center my-5 border-4 rounded-xl">
                 <div className="space-y-4 text-center text-2xl">
-                    <div className="flex gap-3 text-center">
-                        
+                    <div className="flex gap-3">
+                        <ScoreReaction/>
+                        <ScoreReaction image="./images/face-angry-solid.svg" />
+                        <ScoreReaction image="./images/face-angry-solid.svg" />
                     </div>
                 </div>
             </div>
