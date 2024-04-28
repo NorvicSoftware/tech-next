290d93a47547809869fc6d4fe7fe1073ce7a1de3
Americo <americooficial23@gmail.com>
2024-04-27 22:20:17 -0400

    subiendo update

--- app/Http/Controllers/User/UserCareerController.php
+++ app/Http/Controllers/User/UserCareerController.php
@@ -11,10 +11,7 @@ class UserCareerController extends Controller
 {
     public function getCareers(){
         $careers = Career::where('university_id', '=', 1)->get();
-<<<<<<< HEAD
-        //$careers = Career::take(8)->get();
-=======
->>>>>>> 51e6c4a0b267175292ab38e7e1d9421d2b25a166
+
         return Inertia::render('Users/Careers', ['careers' => $careers]);
     }
 }
