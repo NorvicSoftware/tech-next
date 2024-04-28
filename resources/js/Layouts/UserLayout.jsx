1c1661063a007eb24cb1f190eb9e20734b37ce92
Maicol <maicolarratia4@gmail.com>
2024-04-27 22:50:55 -0400

    Update

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -1,9 +1,25 @@
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import { Link } from "@inertiajs/react";
 import ApplicationSun from "@/Components/ApplicationSun";
+import { useEffect, useState } from "react";
 export default function UserLayout({ children }) {
+
+    const [theme, setTheme] = useState("light")
+
+    useEffect(() => {
+        if (theme == "dark") {
+            document.querySelector("html").classList.add('dark')
+        } else {
+            document.querySelector("html").classList.remove('dark')
+        }
+    }, [theme])
+
+    const handleChangeTheme = () => {
+        setTheme(prevTheme => prevTheme == "light" ? "dark" : "light")
+    }
+ 
     return (
-        <div className="min-h-screen bg-gray-300 dark:bg-gray-900">
+        <div class="" className="min-h-screen bg-gray-300 dark:bg-gray-900">
             <nav className="bg-white dark:bg-slate-900 border-b border-gray-100 dark:border-gray-700">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
@@ -19,7 +35,8 @@ export default function UserLayout({ children }) {
                         </div>
 
                         <div className="flex items-center ms-6">
-                            <ApplicationSun className="block h-9 w-auto fill-current text-gr0ay-800 dark:text-gray-20" />
+                            <button onClick={handleChangeTheme} ><ApplicationSun className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-20"  /></button>
+                            
                         </div>
                     </div>
                 </div>
--- resources/js/Pages/Users/Careers.jsx
+++ resources/js/Pages/Users/Careers.jsx
@@ -9,6 +9,7 @@ export default function Career() {
         return `/img/careers/img${careerId}.png`;
     };
 
+
     return (
         <UserLayout>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -16,28 +17,34 @@ export default function Career() {
                     <div className="p-6 text-gray-900 dark:text-gray-100">
                     <div>                    
                         <h2 className=" p-3 text-center" >Inspirate y crea nuevas ideas</h2>
+                        <h2 className=" p-3 text-center" >Elige una carrera:</h2>
                     </div>
-                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-slate-800">
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                             {careers.map((career, index) => (
+                                <a href={route("projects.by.career", {
+                                            careerId: career.id,
+                                        })}
+                                        >
                                 <div
-                                    className="grid justify-items-center border rounded-xl border-white"
+                                    className="grid justify-items-center border rounded-xl border-emerald-500"
                                     key={career.id}
                                 >
                                     <div className="text-center pt-3">
-                                        <img
-                                            className="w-32"
-                                            src={getImagePath(index +1)}
-                                            alt={career.name}
-                                        />
+                                            <img
+                                                className="w-32"
+                                                src={getImagePath(index +1)}
+                                                alt={career.name}
+                                            />
                                     </div>
                                     <LinkButton
                                         name={career.name}
                                         url={route("projects.by.career", {
                                             careerId: career.id,
                                         })}
-                                        className="block bg-slate-800"
+                                        className="block text-green-900 dark:text-white"
                                     />
                                 </div>
+                                </a>
                             ))}
                         </div>
                     </div>
