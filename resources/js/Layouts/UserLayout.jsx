811c36afdc3f8cbf9ac5c1676220a1aa08fa302b
Americo <americooficial23@gmail.com>
2024-04-28 21:20:17 -0400

    subiendo mejoras de imagenes, modo claro y oscuro

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -1,12 +1,18 @@
 import { Link } from "@inertiajs/react";
+import { useState } from "react";
 import ApplicationSun from "@/Components/Icons/Sun";
 import ApplicationMoon from "@/Components/Icons/Moon";
-export default function UserLayout({ children }) {
 
- 
+export default function UserLayout({ children }) {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <div class="" className="min-h-screen bg-gray-300 dark:bg-gray-900">
-            <nav className="bg-white dark:bg-slate-900 border-b border-gray-100 dark:border-gray-700">
+        <div className={`min-h-screen ${darkMode ? 'dark' : ''}`}>
+            <nav className={`bg-white ${darkMode ? 'dark:bg-slate-900' : ''} border-b border-gray-100 dark:border-gray-700`}>
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
@@ -14,16 +20,22 @@ export default function UserLayout({ children }) {
                                 <Link href="/">
                                     <img src="/img/logo-project.png" className="w-11 h-8" />
                                 </Link>
-                                <h2 className="text-1xl font-bold dark:text-gray-200 text-gray-700">
+                                <h2 className={`text-1xl font-bold ${darkMode ? 'dark:text-gray-200 text-gray-700' : ''}`}>
                                     START PROJECT
                                 </h2>
                             </div>
                         </div>
                         <div className="flex items-center ms-6">
-                            <ApplicationSun className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-20"></ApplicationSun>
-                        </div>
-                        <div className="flex items-center ms-6">
-                            <ApplicationMoon className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-20"></ApplicationMoon>
+                            {!darkMode && (
+                                <button onClick={toggleDarkMode}>
+                                    <ApplicationMoon className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-20" />
+                                </button>
+                            )}
+                            {darkMode && (
+                                <button onClick={toggleDarkMode}>
+                                    <ApplicationSun className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-20" />
+                                </button>
+                            )}
                         </div>
                     </div>
                 </div>
