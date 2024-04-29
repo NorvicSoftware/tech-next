8482f033fb8fce794d30319d362648a279c6c748
Americo <americooficial23@gmail.com>
2024-04-28 21:27:06 -0400

    subiendo mejoras del codigo em modo calro y modo oscuro

--- resources/js/Pages/Users/Careers.jsx
+++ resources/js/Pages/Users/Careers.jsx
@@ -12,7 +12,7 @@ export default function Career() {
 
     return (
         <UserLayout>
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 md:mt-10" >
+            <div className="dark:bg-gray-800 max-w-7xl mx-auto sm:px-6 lg:px-8 md:mt-0" >
                 <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 text-gray-900 dark:text-gray-100">
                         <div>
