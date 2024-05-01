f9e8cb3c85f0ffe55f7c09c885a47564ccdf0139
Rey <reycrisbf@gmail.com>
2024-05-01 13:27:38 -0400

    Se realizo la vista de login

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,19 +1,26 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                {/*
+        <div className="min-h-screen sm:justify-center items-center sm:pt-0 bg-slate-800 dark:bg-gray-900">
+            
+            <div className=" p-20 grid grid-cols-2 justify-center items-center">
+            
+                <div className="col-span-1  z-10 w-full">
+                    {/*
                     <Link href="/">
                         <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                     </Link>
                 */}
-            </div>
+                    <img className="w-[400px]" src="img/login-ph.jpg" alt="" />
+                </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+                {
+                    <div className="w-full sm:max-w-md mt-6 dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg col-start-0 col-span-1 z-10">
+                        {children}
+                    </div>
+                }
             </div>
         </div>
     );
