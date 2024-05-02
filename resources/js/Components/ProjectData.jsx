c7a401236f450088358e7a13c0522fbe322059ae
Americo <americooficial23@gmail.com>
2024-05-02 11:53:23 -0400

    subiendo ultimo datos

--- resources/js/Components/ProjectData.jsx
+++ resources/js/Components/ProjectData.jsx
@@ -1,7 +1,7 @@
 export default function ProjectData({ children, name = "", className = "" }) {
     return (
-        <h2 className={"dark:text-black text-black sm:text-xl  " + className}>
-            <span className="dark:text-black text-black">{name} </span>
+        <h2 className={"dark:text-white text-black sm:text-xl  " + className}>
+            <span className="dark:text-white text-black">{name} </span>
             {children}
         </h2>
     );
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -28,7 +28,7 @@ export default function Authenticated({
         setDarkMode(!darkMode);
     };
     return (
-        <div className={`min-h-screen bg-slate-900  ${darkMode ? "dark" : ""}`}>
+        <div className={`min-h-screen ${darkMode ? "bg-slate-900 dark" : "bg-white"}`}>
             <nav className="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
                 <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
